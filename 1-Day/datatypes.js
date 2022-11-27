0982f865dec8c6cdc739c1e580018fe53608fb93
Ankit Sharma <ankitsharma29july@gmail.com>
2022-11-27 16:48:51 +0530

    Day 1 Completed

---
+++ 1-Day/datatypes.js
@@ -0,0 +1,11 @@
+let first_name
+let last_name
+let marital_status
+let age
+let country,status
+
+let myAge = 25
+let yourAge = 30
+console.log('I am',myAge,'years old.')
+console.log('You are',yourAge,'years old.')
+console.log(age=56)
---
+++ 1-Day/variable.js
@@ -0,0 +1,12 @@
+// first javscript in external file
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/*
+comments can make code readable, easy to resuse
+and informative
+*/
+let b = true
+let s = 'Hello Word'
+let c
+let d = null
+console.log(b,s,c,d)
